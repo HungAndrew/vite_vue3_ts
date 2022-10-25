fedd8969c76d935bc280e2c16379deddf1089880
andrew <andrew@cloud-latitude.com>
2022-10-25 10:28:47 +0800

    add tailwindCSS

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -17,7 +17,7 @@ const count = ref(0)
     </p>
   </div>
 
-  <p>
+  <p class="text-3xl font-bold underline">
     Check out
     <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank"
       >create-vue</a
