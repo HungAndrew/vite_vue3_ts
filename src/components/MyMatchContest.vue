81ab4c7791f0fed56cb7233cc3ccc9baad8a34bf
andrew <andrew@cloud-latitude.com>
2022-10-26 21:20:48 +0800

    useRoute

--- src/components/MyMatchContest.vue
+++ src/components/MyMatchContest.vue
@@ -1,10 +1,15 @@
 <script lang="ts" setup>
 
+import {useRoute} from "vue-router";
+
+
+const route = useRoute();
+console.log(route.params.contestCode)
 
 </script>
 
 <template>
-  <div>my match contest</div>
+  <div>my match contest: {{ route.params.contestCode }}</div>
 </template>
 
 
--- src/router/index.ts
+++ src/router/index.ts
@@ -2,16 +2,17 @@ import {createRouter, createWebHashHistory, RouteRecordRaw} from 'vue-router'
 import MyMatchRoute from "./MyMatch";
 
 const routes: Array<RouteRecordRaw> = [
-    {
-        path: "/",
-        name: "Home",
-        component: () => import('../components/Home.vue')
-    },
+
     ...MyMatchRoute,
     {
         path: '/:sport/match/:matchId/detail',
         name: "MatchDetail",
         component: () => import('../components/Home.vue'),
+    },
+    {
+        path: "/",
+        name: "Home",
+        component: () => import('../components/Home.vue')
     },
     {
         path: '/:pathMatch(.*)*',
