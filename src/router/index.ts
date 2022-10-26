4c6b036eae1c147dd37c93ac7bfcaaa6f27aa99b
andrew <andrew@cloud-latitude.com>
2022-10-26 21:24:22 +0800

    reformat

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,17 +2,16 @@ import {createRouter, createWebHashHistory, RouteRecordRaw} from 'vue-router'
 import MyMatchRoute from "./MyMatch";
 
 const routes: Array<RouteRecordRaw> = [
-
+    {
+        path: "/",
+        name: "Home",
+        component: () => import('../components/Home.vue')
+    },
     ...MyMatchRoute,
     {
         path: '/:sport/match/:matchId/detail',
         name: "MatchDetail",
         component: () => import('../components/Home.vue'),
-    },
-    {
-        path: "/",
-        name: "Home",
-        component: () => import('../components/Home.vue')
     },
     {
         path: '/:pathMatch(.*)*',
